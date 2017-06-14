63bfece5fe2587d5a67be85120c0247ac59fbb72
George Albuquerque <georgealbuquerquepinto@hotmail.com>
2017-06-14 20:28:12 -0300

    Update

--- view/pyqt.py
+++ view/pyqt.py
@@ -2,39 +2,40 @@ from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 from PyQt4.QtWebKit import *
 
-maphtml = '''
+latitude = -3.685266
+longitude = -40.3461367
+
+maphtml = """
 <!DOCTYPE html>
 <html>
   <head>
     <style>
-       #map {
+       #map {{
         height: 400px;
         width: 100%;
-       }
+       }}
     </style>
   </head>
   <body>
     <div id="map"></div>
     <script>
-      function initMap() {
-        var uluru = {lat: -3.685266, lng: -40.3461367};
-        var map = new google.maps.Map(document.getElementById('map'), {
+      function initMap() {{
+        var uluru = {{lat: {lat}, lng: {lon}}};
+        var map = new google.maps.Map(document.getElementById('map'), {{
           zoom: 16,
           center: uluru
-        });
-        var marker = new google.maps.Marker({
+        }});
+        var marker = new google.maps.Marker({{
           position: uluru,
           map: map
-        });
-      }
+        }});
+      }}
     </script>
     <script async defer
     src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAb1YqoLx0LC2GECyyOXhbCe2kDo8zRyCY&callback=initMap">
     </script>
   </body>
-</html>
-'''
-
+  </html>""".format(lat=latitude,lon=longitude)
 class Browser(QApplication):
     def __init__(self):
         QApplication.__init__(self, [])
